0c5ed45587b507458e17f19cb33778343b78ddad
Sergei Luckyday <sergeiluckyday@gmail.com>
2025-05-16 23:28:45 +0600

    pp 2.1.4

--- src/main/java/koschei/config/AppConfig.java
+++ src/main/java/koschei/config/AppConfig.java
@@ -1,7 +1,6 @@
 package koschei.config;
 
-import koschei.models.Island2;
-import koschei.models.Wood3;
+import koschei.models.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -13,5 +12,20 @@ public class AppConfig {
     @Bean
     public static Island2 getIsland(Wood3 wood) {
         return new Island2(wood);
+    }
+
+    @Bean
+    public Deth8 getDeath8() {
+        return new Deth8();
+    }
+
+    @Bean
+    public Needle7 getNeedle7(Deth8 deth8) {
+        return new Needle7(deth8);
+    }
+
+    @Bean
+    public Egg6 getEgg6(Needle7 needle7) {
+        return new Egg6(needle7);
     }
 }
